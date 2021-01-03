958ae30194c8db49789aec68e91cc710cbb5397a
elenapop <72506899+elenapop@users.noreply.github.com>
2021-01-03 21:38:06 +0200

    Add files via upload

---
+++ Pop_Elena_Lab10/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Pop_Elena_Lab10.Models
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        public string Description { get; set; }
+
+        public List<ListProduct> ListProducts { get; set; }
+
+    }
+}
